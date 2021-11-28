925bed0acccdb333fb1515bcfcee26918fd04c55
t3rry <teriyakiisacookery@gmail.com>
2021-11-28 17:37:22 +0900

    create state sequence controller

--- lib/presentation/controller/block_controller.dart
+++ lib/presentation/controller/block_controller.dart
@@ -4,8 +4,25 @@ import 'package:shogi_note/domain/model/board_state.dart';
 
 class BlockController with ChangeNotifier {
   final Block block;
+  int stateIndex;
 
-  BlockController({required this.block});
+  BlockController({required this.block, this.stateIndex = 0});
 
-  BoardState get boardState => block.boardStateList[0];
+  BoardState get boardState => block.boardStateList[stateIndex];
+
+  void onClickNextButton() {
+    if (stateIndex >= block.boardStateList.length - 1) {
+      return;
+    }
+    stateIndex += 1;
+    notifyListeners();
+  }
+
+  void onClickBackButton() {
+    if (stateIndex <= 0) {
+      return;
+    }
+    stateIndex -= 1;
+    notifyListeners();
+  }
 }
